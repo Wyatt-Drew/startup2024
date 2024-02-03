5e6ee21bbd010252dc4b0eca6262deee4dda312e
wyatt-drew <wyatt.j.drew@gmail.com>
2024-02-03 02:59:00 -0600

    Updated login

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,17 +6,8 @@ const Footer = () => {
         <footer className="footer">
             <div className="footer-content">
                 <div className="footer-section about">
-                    <h2>Company Name</h2>
-                    <p>Description of your company or footer message. This section can include a brief about the company or any additional information you wish to include.</p>
-                </div>
-                <div className="footer-section links">
-                    <h2>Quick Links</h2>
-                    <ul>
-                        <li>About Us</li>
-                        <li>Services</li>
-                        <li>Privacy Policy</li>
-                        <li>Contact Us</li>
-                    </ul>
+                    <h2>Aureus Database</h2>
+                    <p>A database designed to expand the current understanding of antibiotic resistant bacterial infections by tracking how outcomes correlate with prescriptions.</p>
                 </div>
                 <div className="footer-section contact">
                     <h2>Contact</h2>
@@ -25,7 +16,7 @@ const Footer = () => {
                 </div>
             </div>
             <div className="footer-bottom">
-                &copy; {new Date().getFullYear()} Company Name | All Rights Reserved
+                &copy; {new Date().getFullYear()} Aureus Database | All Rights Reserved
             </div>
         </footer>
     );
