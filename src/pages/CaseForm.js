416b2691ce481d4c498e5f531dbf2dc9c2583d43
wyatt-drew <wyatt.j.drew@gmail.com>
2024-02-03 01:50:36 -0600

    Dashboard link

--- src/pages/CaseForm.js
+++ src/pages/CaseForm.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
-import { useLocation } from 'react-router-dom'; // Import useLocation
+import { useLocation, useNavigate } from 'react-router-dom'; // Import useNavigate here
 import './LabDataForm.css';
 
 const CaseForm = () => {
   const location = useLocation(); // Use the useLocation hook
+  const navigate = useNavigate(); // Initialize useNavigate
   const caseDataFromDashboard = location.state || {}; // Access the state
 
   const [caseData, setCaseData] = useState({
@@ -14,29 +15,30 @@ const CaseForm = () => {
     ...caseDataFromDashboard // Spread in the caseDataFromDashboard to override defaults
   });
   
-    const handleChange = (event) => {
-      setCaseData({
-        ...caseData,
-        [event.target.name]: event.target.value,
-      });
-    };
+  const handleChange = (event) => {
+    setCaseData({
+      ...caseData,
+      [event.target.name]: event.target.value,
+    });
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log('Submitting Case Data:', caseData);
     // Additional submission logic here
+    
+    // After form submission, navigate back to the Dashboard
+    navigate('/Dashboard');
   };
 
-  // Use the caseData state for displaying disease and site
   return (
     <div className="lab-data-form-container">
       <form onSubmit={handleSubmit} className="lab-data-form">
         <h2>Case Submission</h2>
-        {/* Dynamically display disease and site */}
         <h3>Disease: {caseData.disease || 'N/A'}</h3>
         <h3>Site: {caseData.site || 'N/A'}</h3>
 
-        <div className="input-group-row"> {/* Updated class name for styling */}
+        <div className="input-group-row">
           <div className="lab-data-group">
             <label>Treatment Prescribed:</label>
             <input
@@ -61,14 +63,14 @@ const CaseForm = () => {
             />
           </div>
         </div>
-        <div className="lab-data-group"> {/* Adjusted for Outcome to match other inputs */}
+        <div className="lab-data-group">
             <label>Outcome:</label>
             <input
               type="text"
               id="outcome"
               name="outcome"
               placeholder="Outcome"
-              value={caseData.outcome} // Managed state for outcome
+              value={caseData.outcome}
               onChange={handleChange}
               required
             />
